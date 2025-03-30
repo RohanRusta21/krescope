7d6c7df1f81c680fc427994e7e1190e8e072df64
Rohan Rustagi <110477025+RohanRusta21@users.noreply.github.com>
2025-03-30 23:02:40 +0530

    Update setup.py

--- krescope/setup.py
+++ krescope/setup.py
@@ -1,1 +1,14 @@
 
+from setuptools import setup, find_packages
+
+setup(
+    name="k8s-recommender",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["click", "kubernetes", "rich"],
+    entry_points={
+        "console_scripts": [
+            "krescope=recommender.cli:cli"
+        ]
+    }
+)
